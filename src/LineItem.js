fee4ac916ef941f7000d1ba9f48ca80617c2ab35
codepark <adriansudak@outlook.com>
2023-07-14 15:30:20 +0200

    First and last commit

---
+++ src/LineItem.js
@@ -0,0 +1,21 @@
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item" key={item.id}>
+      <input
+        onChange={() => handleCheck(item.id)}
+        type="checkbox"
+        checked={item.checked}
+      />
+      <label onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+      />
+    </li>
+  );
+};
+
+export default LineItem;
